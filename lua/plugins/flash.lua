68bfc23adaf91d818e83467b7e5e732843bb54e7
Guga Guichard <gustavoguichard@gmail.com>
2024-12-07 08:22:05 -0300

    Remap flash to gs instead of s

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "gs",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      { "s", false },
+    },
+  },
+}
